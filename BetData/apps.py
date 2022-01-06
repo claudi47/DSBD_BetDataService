bd961ccf1e69f3cc8945cb06a4873de3bf12d763
Alessandro Bellia <casildaserrano@yahoo.it>
2022-01-06 23:53:25 +0100

    fixed kafka dependencies and testing
    Signed-off-by: Alessandro Bellia <casildaserrano@yahoo.it>

--- BetData/apps.py
+++ BetData/apps.py
@@ -1,31 +1,8 @@
-import multiprocessing
 import sys
 
-from confluent_kafka import Consumer
 from django.apps import AppConfig
 
 from BetData.transaction_scheduler import init_scheduler
-
-
-def kafka_consumer():
-    consumer = Consumer({
-        'bootstrap.servers': 'kafka:9092',
-        'group.id': 'LOLconsumer'
-    })
-    consumer.subscribe(['test'])
-    timeout = 5.0
-    while True:
-        msg = consumer.poll(timeout)
-        if msg is None:
-            consumer.close()
-            return None
-        if msg.value() != 'ok':
-            continue
-        if msg.error():
-            print(msg.error())
-            continue
-        print(msg.value().decode('utf-8'))
-
 
 class BetdataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -35,4 +12,3 @@ class BetdataConfig(AppConfig):
     def ready(self):
         if not 'migrate' in sys.argv:
             init_scheduler()
-            multiprocessing.Process(target=kafka_consumer).start()
